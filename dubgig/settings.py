6cb9132178fcc9d448320dca7f4ebe66688fdc04
DanielMaherDev <dmdanielmaher@gmail.com>
2023-02-08 20:49:08 +0000

    connect to elephantsql database

--- dubgig/settings.py
+++ dubgig/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'dubgig.wsgi.application'
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
+#     } 
 # }
 
 DATABASES = {
